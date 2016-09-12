ce2e02ae257f24ee9a839495af0887f39894a971
Paul Mai <dai.maithanh@gmail.com>
2016-09-12 18:01:42 +0700

    New implement of get TOTP

--- src/main/java/com/lasso/util/EncryptionUtil.java
+++ src/main/java/com/lasso/util/EncryptionUtil.java
@@ -3,7 +3,9 @@
  */
 package com.lasso.util;
 
+import java.lang.reflect.UndeclaredThrowableException;
 import java.nio.ByteBuffer;
+import java.security.GeneralSecurityException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -11,10 +13,12 @@ import java.security.spec.AlgorithmParameterSpec;
 import java.security.spec.KeySpec;
 
 import javax.crypto.Cipher;
+import javax.crypto.Mac;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.PBEParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.log4j.Logger;
@@ -28,6 +32,11 @@ public final class EncryptionUtil {
 
 	/** The Constant DEFAULT_PASSWORD. */
 	private static final String	DEFAULT_PASSWORD	= "phskyd5NjS";
+
+	/** The Constant DIGITS_POWER. */
+	private static final int[] DIGITS_POWER
+	// 0 1 2 3 4 5 6 7 8
+	= { 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000 };
 
 	/** The Constant ITERATION_COUNT. */
 	private final static int	ITERATION_COUNT		= 31;
@@ -177,6 +186,75 @@ public final class EncryptionUtil {
 	}
 
 	/**
+	 * Generate TOTP.
+	 *
+	 * @return the int
+	 */
+	public static int generateTOTP() {
+		return EncryptionUtil.generateTOTP("RuVTZYpBUf9bC58Ptrgp".getBytes());
+	}
+
+	/**
+	 * Generate TOTP.
+	 *
+	 * @param key the key
+	 * @return the int
+	 */
+	public static int generateTOTP(byte[] key) {
+		return EncryptionUtil.generateTOTP(key, System.currentTimeMillis() / 1000 / 30);
+	}
+
+	/**
+	 * Generate TOTP.
+	 *
+	 * @param key the key
+	 * @param time the time
+	 * @return the int
+	 */
+	public static int generateTOTP(byte[] key, long time) {
+		return EncryptionUtil.generateTOTP(key, time, 6);
+	}
+
+	/**
+	 * Generate TOTP.
+	 *
+	 * @param key the key
+	 * @param time the time
+	 * @param digits the digits
+	 * @return the int
+	 */
+	public static int generateTOTP(byte[] key, long time, int digits) {
+		return EncryptionUtil.generateTOTP(key, time, digits, "HmacSHA1");
+	}
+
+	/**
+	 * This method generates a TOTP value for the given set of parameters.
+	 *
+	 * @param key : the shared secret
+	 * @param time : a value that reflects a time
+	 * @param digits : number of digits to return
+	 * @param crypto : the crypto function to use (HmacSHA1, HmacSHA256, HmacSHA512)
+	 * @return the int
+	 * @return: digits
+	 */
+
+	public static int generateTOTP(byte[] key, long time, int digits, String crypto) {
+
+		byte[] msg = ByteBuffer.allocate(8).putLong(time).array();
+		byte[] hash = EncryptionUtil.hmacSha(crypto, key, msg);
+
+		// put selected bytes into result int
+		int offset = hash[hash.length - 1] & 0xf;
+
+		int binary = ((hash[offset] & 0x7f) << 24) | ((hash[offset + 1] & 0xff) << 16)
+				| ((hash[offset + 2] & 0xff) << 8) | (hash[offset + 3] & 0xff);
+
+		int otp = binary % EncryptionUtil.DIGITS_POWER[digits];
+
+		return otp;
+	}
+
+	/**
 	 * *
 	 * Copy of uniqid in php http://php.net/manual/fr/function.uniqid.php
 	 *
@@ -203,9 +281,32 @@ public final class EncryptionUtil {
 	}
 
 	/**
+	 * This method uses the JCE to provide the crypto algorithm. HMAC computes a
+	 * Hashed Message Authentication Code with the crypto hash algorithm as a
+	 * parameter.
+	 *
+	 * @param crypto : the crypto algorithm (HmacSHA1, HmacSHA256, HmacSHA512)
+	 * @param keyBytes : the bytes to use for the HMAC key
+	 * @param text : the message or text to be authenticated
+	 * @return the byte[]
+	 */
+
+	private static byte[] hmacSha(String crypto, byte[] keyBytes, byte[] text) {
+		try {
+			Mac hmac;
+			hmac = Mac.getInstance(crypto);
+			SecretKeySpec macKey = new SecretKeySpec(keyBytes, "RAW");
+			hmac.init(macKey);
+			return hmac.doFinal(text);
+		}
+		catch (GeneralSecurityException gse) {
+			throw new UndeclaredThrowableException(gse);
+		}
+	}
+
+	/**
 	 * Instantiates a new encryption utils.
 	 */
 	private EncryptionUtil() {
 	}
-
 }
---
+++ src/test/java/TestOtp.java
@@ -0,0 +1,24 @@
+import com.lasso.util.EncryptionUtil;
+
+/**
+ * The Class TestOtp.
+ *
+ * @author Paul Mai
+ */
+public class TestOtp {
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 * @throws InterruptedException the interrupted exception
+	 */
+	public static void main(String[] args) throws InterruptedException {
+		for (int _i = 0; _i < 60; _i++) {
+			System.out.println(EncryptionUtil.generateTOTP());
+			Thread.sleep(1000);
+		}
+
+	}
+
+}
